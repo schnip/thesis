23046041117e44d4934caeb8759678f1294bad7b
schnip <pingthegreenflag@gmail.com>
2017-08-31 21:49:43 -0400

    calculations for individual parts

--- scrap/mutation_guess_estimate.py
+++ scrap/mutation_guess_estimate.py
@@ -4,3 +4,15 @@ chars_available = 128
 # No single removal can make the code work perfectly
 sing_rem = 0
 sing_rem_prob = sing_rem / char_cnt
+
+# Single add can fix it by adding a comment
+# I cannot think of any other ways that it can be done with a single add
+sing_add = 3
+sing_add_prob = sing_add / (char_cnt * chars_available)
+
+# Swap the 1 to a 0
+# Swap two of the different ones to a hash
+# Swap plus to mult
+# Swap plus to divide
+sing_swap = 1 + 2 + 1 + 1
+sing_swap_prob = sing_swap / (char_cnt * chars_available)
